801c20702da5d24a97291111e26a51548681d2a1
Bonaventure C. Mbbah-Offor <bonaventurembbahoffor@gmail.com>
2025-08-20 15:54:59 +0100

    fix: attempting to fix a 502 bad gateway error upon visiting the url

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ app.post("/notes", async (req, res) => {
   res.json(result.rows[0]);
 });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;;
 app.listen(PORT, () =>
   console.log(`✅ Server running at http://localhost:${PORT}`)
 );
